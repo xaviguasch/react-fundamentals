1e5ca1afc35e0356c00dec1b9e07427ac7cb699b
xaviguasch <xaviguasch@gmail.com>
2020-10-06 23:35:22 +0200

    added solution for FORMS extra credit 3

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -70,31 +70,70 @@ function UsernameForm({onSubmitUsername}) {
 
   // EXTRA CREDIT 2
 
-  const [error, setError] = React.useState(null)
+  //   const [error, setError] = React.useState(null)
+
+  //   function handleSubmit(event) {
+  //     event.preventDefault()
+  //     onSubmitUsername(event.target.elements.usernameInput.value)
+  //   }
+
+  //   function handleChange(event) {
+  //     const {value} = event.target
+  //     const isLowerCase = value === value.toLowerCase()
+  //     setError(isLowerCase ? null : 'Username must be lower case')
+  //   }
+
+  //   return (
+  //     <form onSubmit={handleSubmit}>
+  //       <div>
+  //         <label htmlFor="usernameInput">Username:</label>
+  //         <input id="usernameInput" type="text" onChange={handleChange} />
+  //       </div>
+  //       <div role="alert" style={{color: 'red'}}>
+  //         {error}
+  //       </div>
+  //       <button disabled={Boolean(error)} type="submit">
+  //         Submit
+  //       </button>
+  //     </form>
+  //   )
+  // }
+
+  // function App() {
+  //   const onSubmitUsername = username => alert(`You entered: ${username}`)
+  //   return (
+  //     <div style={{minWidth: 400}}>
+  //       <UsernameForm onSubmitUsername={onSubmitUsername} />
+  //     </div>
+  //   )
+  // }
+
+  // EXTRA CREDIT 3
+
+  const [username, setUsername] = React.useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
-    onSubmitUsername(event.target.elements.usernameInput.value)
+    onSubmitUsername(username)
   }
 
   function handleChange(event) {
     const {value} = event.target
-    const isLowerCase = value === value.toLowerCase()
-    setError(isLowerCase ? null : 'Username must be lower case')
+    setUsername(value.toLowerCase())
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input id="usernameInput" type="text" onChange={handleChange} />
+        <input
+          id="usernameInput"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
       </div>
-      <div role="alert" style={{color: 'red'}}>
-        {error}
-      </div>
-      <button disabled={Boolean(error)} type="submit">
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
